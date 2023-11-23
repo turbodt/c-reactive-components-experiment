ef17112bbdedc991102864f840ffcb633627bc34
Turbodt <turbodt@localhost>
2023-11-23 16:31:54 +0100

    Main example improve rendering system

--- src/screen.c
+++ src/screen.c
@@ -5,26 +5,28 @@
 #include <string.h>
 
 
-#define SCREEN_MAX_COLS 80
-#define SCREEN_MAX_ROWS 96
+#define SCREEN_MAX_COLS 256
+#define SCREEN_MAX_ROWS 128
 #define SCREEN_MAX_LINE_SIZE 1024
 
 #define MAX(a, b) ((a) < (b) ? (b) : (a))
 #define MIN(a, b) ((a) > (b) ? (b) : (a))
 
+#define VAL_INDEX(size, i, j) ((i) * (size)->cols + (j))
 
 struct Screen {
     FILE * out;
     ScreenSize size;
-    char values[SCREEN_MAX_ROWS][SCREEN_MAX_COLS + 1];
-    ScreenCoordinates rendered_at_offset;
+    char * next_values;
+    char * last_values;
 };
 
 
-static void screen_init(Screen *, FILE *, ScreenSize const);
+static void screen_init(Screen *, FILE *, ScreenSize const *);
 static void screen_clean_up(Screen *);
-static void screen_clear(Screen *);
-static void screen_render_line(Screen *, size_t, int);
+static void screen_clear_next_values(Screen *);
+static void screen_clear_last_values(Screen *);
+static void screen_render_line(Screen *, size_t);
 static void screen_render_header(Screen *);
 static void screen_render_footer(Screen *);
 static void remove_header(void);
@@ -32,7 +34,7 @@ static void remove_footer(void);
 static void remove_last_line(void);
 
 
-Screen * screen_alloc(FILE * out, ScreenSize const size) {
+Screen * screen_alloc(FILE * out, ScreenSize const * size) {
     Screen * screen = XRE_ALLOC(Screen, 1);
     XRE_ASSERT_ALLOC(screen);
 
@@ -50,80 +52,82 @@ void screen_destroy(Screen *screen) {
 };
 
 
-void screen_render_at(Screen *screen, ScreenCoordinates const offset) {
-    ScreenSize const size = screen->size;
+void screen_render(Screen *screen) {
+    ScreenSize const * size = screen_get_size(screen);
+
+    if (strcmp(screen->last_values, screen->next_values) == 0) {
+        return;
+    }
+
+    screen_render_clear(screen);
 
     screen_render_header(screen);
-    for (size_t i = 0; i < size.rows + offset.y && i < SCREEN_MAX_ROWS; i++) {
-        screen_render_line(screen, i, offset.x);
-        fprintf(screen->out, "\n");
+    for (size_t i = 0; i < size->rows; i++) {
+        screen_render_line(screen, i);
     }
     screen_render_footer(screen);
 
-    screen_clear(screen);
-    screen->rendered_at_offset = offset;
+    screen_clear_last_values(screen);
+
+    char * swap_value = screen->next_values;
+    screen->next_values = screen->last_values;
+    screen->last_values = swap_value;
 };
 
 
-inline void screen_render(Screen *screen) {
-    screen_render_at(screen, (ScreenCoordinates){0,0});
-}
+void screen_render_clear(Screen * screen) {
+    ScreenSize const * size = screen_get_size(screen);
 
-void screen_clean(Screen * screen) {
-    ScreenCoordinates const offset = screen->rendered_at_offset;
-    ScreenSize const size = screen->size;
     remove_header();
-    for (size_t i = 0; i < size.rows + offset.y && i < SCREEN_MAX_ROWS; i++) {
+    for (size_t i = 0; i < size->rows; i++) {
         remove_last_line();
     }
     remove_header();
-
-    screen->rendered_at_offset = (ScreenCoordinates){0,0};
 };
 
 
 void screen_vprintf(
     Screen * screen,
-    ScreenCoordinates const coords,
+    ScreenCoordinates const * coords,
     char const * format,
     va_list props
 ) {
-    ScreenSize const size = screen->size;
-    if (coords.y < 0 || coords.y >= (int) size.rows) {
+    ScreenCoordinates final_coords;
+    ScreenSize const * size = screen_get_size(screen);
+
+    if (coords->y < 0 || coords->y >= (int) size->rows) {
         return;
     }
 
-    char buffer[SCREEN_MAX_COLS];
-    vsnprintf(buffer, SCREEN_MAX_COLS, format, props);
+    char buffer[size->rows];
+    vsnprintf(buffer, size->cols, format, props);
     size_t str_len = strlen(buffer);
 
-    if (coords.x + (int) str_len < 0 || coords.x >= (int) size.cols) {
+    if (coords->x + (int) str_len < 0 || coords->x >= (int) size->cols) {
         return;
     }
 
-    size_t str_final_len = MIN(str_len, str_len + coords.x);
+    size_t str_final_len = MIN(str_len, str_len + coords->x);
     if (str_final_len <= 0) {
         return;
     }
 
-    ScreenCoordinates final_coords = {
-        MAX((int) 0, coords.x),
-        coords.y
-    };
+    final_coords.x = MAX((int) 0, coords->x);
+    final_coords.y = coords->y;
 
     size_t str_offset = str_len - str_final_len;
 
     strncpy(
-        screen->values[final_coords.y] + final_coords.x,
+        screen->next_values + VAL_INDEX(size, final_coords.y, final_coords.x),
         buffer + str_offset,
-        MIN((int) str_final_len, (int) size.cols - final_coords.x)
+        MIN((int) str_final_len, (int) size->cols - final_coords.x)
     );
 };
 
 
 void screen_printf(
     Screen * screen,
-    ScreenCoordinates const coords,
+    ScreenCoordinates const * coords,
     char const * format,
     ...
 ) {
@@ -134,32 +138,60 @@ void screen_printf(
 };
 
 
+inline ScreenSize const * screen_get_size(Screen const *screen) {
+    return &screen->size;
+};
+
+
 //------------------------------------------------------------------------------
 // Private
 //------------------------------------------------------------------------------
 
 
-void screen_init(Screen * screen, FILE * out, ScreenSize const size) {
-    screen->rendered_at_offset = (ScreenCoordinates){0,0};
+void screen_init(Screen * screen, FILE * out, ScreenSize const * size) {
     screen->out = out;
-    screen->size = size;
-    screen_clear(screen);
+
+    screen->size.rows = MIN(SCREEN_MAX_ROWS, size->rows);
+    screen->size.cols = MIN(SCREEN_MAX_COLS, size->cols);
+    size_t len = screen->size.rows * screen->size.cols;
+
+    screen->next_values = XRE_ALLOC(char, len + 1);
+    XRE_ASSERT_ALLOC(screen->next_values);
+
+    screen->last_values = XRE_ALLOC(char, len + 1);
+    XRE_ASSERT_ALLOC(screen->next_values);
+
+    screen_clear_next_values(screen);
+    screen_clear_last_values(screen);
 };
 
 
 inline void screen_clean_up(Screen *screen) {
-    screen_clear(screen);
+    screen_clear_next_values(screen);
+
+    XRE_FREE(screen->next_values);
+    XRE_FREE(screen->last_values);
 };
 
 
-inline void screen_render_line(Screen * screen, size_t row_index, int x) {
-    fprintf(screen->out, "║%s║", screen->values[row_index] + x);
+inline void screen_render_line(Screen * screen, size_t row_index) {
+    ScreenSize const * size = screen_get_size(screen);
+    fprintf(screen->out, "║");
+    fwrite(
+        screen->next_values + VAL_INDEX(size, row_index, 0),
+        sizeof(char),
+        size->cols,
+        screen->out
+    );
+    fprintf(screen->out, "║\n");
 };
 
 
 inline void screen_render_header(Screen * screen) {
+    ScreenSize const * size = screen_get_size(screen);
+
     fprintf(screen->out, "╔");
-    for (size_t i = 0; i < SCREEN_MAX_COLS; i++) {
+    for (size_t i = 0; i < size->cols; i++) {
         fprintf(screen->out, "═");
     }
     fprintf(screen->out, "╗\n");
@@ -170,8 +202,10 @@ inline void remove_header(void) {
 
 
 inline void screen_render_footer(Screen * screen) {
+    ScreenSize const * size = screen_get_size(screen);
+
     fprintf(screen->out, "╚");
-    for (size_t i = 0; i < SCREEN_MAX_COLS; i++) {
+    for (size_t i = 0; i < size->cols; i++) {
         fprintf(screen->out, "═");
     }
     fprintf(screen->out, "╝\n");
@@ -180,13 +214,22 @@ inline void remove_footer(void) {
     remove_last_line();
 };
 
-void screen_clear(Screen *screen) {
-    for (size_t i = 0; i < SCREEN_MAX_ROWS; i++) {
-        for (size_t j = 0; j < SCREEN_MAX_COLS; j++) {
-            screen->values[i][j] = ' ';
-        }
-        screen->values[i][SCREEN_MAX_COLS] = '\0';
-    }
+
+void screen_clear_next_values(Screen *screen) {
+    ScreenSize const * size = screen_get_size(screen);
+    size_t len = VAL_INDEX(size, size->rows, 0);
+
+    memset(screen->next_values, ' ', len);
+    *(screen->next_values + len) = '\0';
+};
+
+
+void screen_clear_last_values(Screen *screen) {
+    ScreenSize const * size = screen_get_size(screen);
+    size_t len = VAL_INDEX(size, size->rows, 0);
+
+    memset(screen->last_values, ' ', len);
+    *(screen->last_values + len) = '\0';
 };
 
 
--- src/screen.h
+++ src/screen.h
@@ -23,15 +23,14 @@ struct ScreenSize {
 typedef struct ScreenSize ScreenSize;
 
 
-Screen * screen_alloc(FILE *, ScreenSize const);
+Screen * screen_alloc(FILE *, ScreenSize const *);
 void screen_destroy(Screen *);
 
 
 void screen_render(Screen *);
-void screen_render_at(Screen *, ScreenCoordinates const);
-void screen_clean(Screen *);
-void screen_printf(Screen *, ScreenCoordinates const, char const *, ...);
-void screen_vprintf(Screen *, ScreenCoordinates const, char const *, va_list);
-
+void screen_render_clear(Screen *);
+void screen_printf(Screen *, ScreenCoordinates const *, char const *, ...);
+void screen_vprintf(Screen *, ScreenCoordinates const *, char const *, va_list);
+ScreenSize const * screen_get_size(Screen const *);
 
 #endif
