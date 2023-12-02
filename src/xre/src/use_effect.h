ba57b2f943f5d765a272d435cae6b89cbe53bac9
Turbodt <turbodt@localhost>
2023-12-02 21:32:55 +0100

    Added use effect

---
+++ src/xre/src/use_effect.h
@@ -0,0 +1,35 @@
+#ifndef XRE_USE_EFFECT_H
+#define XRE_USE_EFFECT_H
+
+
+#include "./context/main.h"
+#include "./use_ref.h"
+#include <stdarg.h>
+
+
+typedef void (*XREEffectCleanUp)(va_list);
+typedef void (*XREEffect)(va_list); // TODO: return clean up
+
+
+struct XREEffectRef {
+    struct XRERef base;
+};
+
+
+struct XREEffectRef * xre_use_veffect(
+    struct XREContext *,
+    XREEffect,
+    struct XRERef const * const [],
+    va_list
+);
+
+
+struct XREEffectRef * xre_use_effect(
+    struct XREContext *,
+    XREEffect,
+    struct XRERef const * const[],
+    ...
+);
+
+
+#endif
