9faa08654240e5be7a1b9950650468adf2707a78
Turbodt <turbodt@localhost>
2024-07-03 23:58:30 +0200

    feature(use): Swap arguments order for context and key

--- src/use.h
+++ src/use.h
@@ -8,9 +8,9 @@
 void xre_use_root(struct XREContext *, XREComponent, ...);
 void xre_vuse_root(struct XREContext *, XREComponent, va_list);
 
-void xre_use(char const *, struct XREContext *, XREComponent, ...);
-void xre_use_ikey(int, struct XREContext *, XREComponent, ...);
-void xre_vuse(char const *, struct XREContext *, XREComponent, va_list);
+void xre_use(struct XREContext *, char const *, XREComponent, ...);
+void xre_use_ikey(struct XREContext *, int, XREComponent, ...);
+void xre_vuse(struct XREContext *, char const *, XREComponent, va_list);
 
 
 #endif
